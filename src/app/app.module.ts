f786aa9abe6ef77f18ba79316f413a2b12fd52e2
Mark Lauron <malauron@gmail.com>
2022-06-14 07:21:44 +0800

    Added item search module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,6 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
-import { ItemsService } from './services/items.service';
-// import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-// import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-
-
-// export function createTranslateLoader(http: HttpClient) {
-//   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
-// }
 
 @NgModule({
   declarations: [AppComponent],
@@ -24,15 +16,8 @@ import { ItemsService } from './services/items.service';
     HttpClientModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    // TranslateModule.forRoot({
-    //   loader: {
-    //     provide: TranslateLoader,
-    //     useFactory: (createTranslateLoader),
-    //     deps: [HttpClient]
-    //   }
-    // })
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy } ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
