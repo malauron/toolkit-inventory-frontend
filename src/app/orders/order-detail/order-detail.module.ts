ebcc392cce32e6568af6a3940b287d64d7a4bd7d
Mark Lauron <malauron@gmail.com>
2022-08-15 05:27:40 +0800

    Incremental updates - Reports

--- src/app/orders/order-detail/order-detail.module.ts
+++ src/app/orders/order-detail/order-detail.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
+import { NgxPrintModule } from 'ngx-print';
 import { OrderDetailPageRoutingModule } from './order-detail-routing.module';
-
 import { OrderDetailPage } from './order-detail.page';
 
 @NgModule({
@@ -13,6 +11,7 @@ import { OrderDetailPage } from './order-detail.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    NgxPrintModule,
     OrderDetailPageRoutingModule
   ],
   declarations: [OrderDetailPage]
