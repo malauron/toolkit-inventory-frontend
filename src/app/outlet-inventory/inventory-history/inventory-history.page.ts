cdb5759829d01e6f044585e9c4783b3cace20567
Mark Lauron <malauron@gmail.com>
2023-05-09 18:03:59 +0800

    Fix duplicate inventory history records.

--- src/app/outlet-inventory/inventory-history/inventory-history.page.ts
+++ src/app/outlet-inventory/inventory-history/inventory-history.page.ts
@@ -51,30 +51,30 @@ export class InventoryHistoryPage implements OnInit, OnDestroy {
   ngOnInit() {
     this.warehouse = new Warehouse();
 
-    this.searchBarSub = this.searchBar.ionInput
-      .pipe(
-        map((event) => (event.target as HTMLInputElement).value),
-        debounceTime(this.config.waitTime),
-        distinctUntilChanged()
-      )
-      .subscribe((res) => {
-        if (this.warehouse.warehouseId) {
-          this.searchValue = res.trim();
-          this.infiniteScroll.disabled = false;
-          this.inventoryHistories = [];
-          this.pageNumber = 0;
-          this.totalPages = 0;
-          this.getInventoryHistories(
-            undefined,
-            this.searchValue,
-            this.warehouse.warehouseId,
-            this.pageNumber,
-            this.config.pageSize
-          );
-        } else {
-          console.log('no data');
-        }
-      });
+    // this.searchBarSub = this.searchBar.ionInput
+    //   .pipe(
+    //     map((event) => (event.target as HTMLInputElement).value),
+    //     debounceTime(this.config.waitTime),
+    //     distinctUntilChanged()
+    //   )
+    //   .subscribe((res) => {
+    //     if (this.warehouse.warehouseId) {
+    //       this.searchValue = res.trim();
+    //       this.infiniteScroll.disabled = false;
+    //       this.inventoryHistories = [];
+    //       this.pageNumber = 0;
+    //       this.totalPages = 0;
+    //       this.getInventoryHistories(
+    //         undefined,
+    //         this.searchValue,
+    //         this.warehouse.warehouseId,
+    //         this.pageNumber,
+    //         this.config.pageSize
+    //       );
+    //     } else {
+    //       console.log('no data');
+    //     }
+    //   });
   }
 
   onPrintView() {
@@ -139,9 +139,7 @@ export class InventoryHistoryPage implements OnInit, OnDestroy {
       .subscribe((res) => {
         res._embedded.inventoryHistories.forEach((item) => {
           const invItem = new InventoryItemDto();
-          this.inventoryHistories = this.inventoryHistories.concat(res._embedded.inventoryHistories);
-
-          console.log(1);
+          this.inventoryHistories = this.inventoryHistories.concat(item);
         });
         this.totalPages = res.page.totalPages;
         this.isFetching = false;
