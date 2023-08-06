c28794f6ba424fd7c22829c313a7a3b83447f0c1
Mark Lauron <malauron@gmail.com>
2023-08-07 07:13:15 +0800

    Incremental Updates

---
+++ src/app/butchery/butchery-batches/butchery-batch-inventory-item-search/butchery-batch-inventory-item-search.component.ts
@@ -0,0 +1,134 @@
+/* eslint-disable no-underscore-dangle */
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { IonSearchbar, ModalController, ViewDidEnter } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
+import { AppParamsConfig } from 'src/app/Configurations/app-params.config';
+import { ButcheryBatchesService } from '../../services/butchery-batches.service';
+import { ButcheryBatch } from '../../classes/butchery-batch.model';
+
+@Component({
+  selector: 'app-butchery-batch-inventory-item-search',
+  templateUrl: './butchery-batch-inventory-item-search.component.html',
+  styleUrls: ['./butchery-batch-inventory-item-search.component.scss'],
+})
+export class ButcheryBatchInventoryItemSearchComponent implements OnInit, OnDestroy {
+  @ViewChild('infiniteScroll') infiniteScroll;
+  @ViewChild('itemSearchBar', { static: true }) itemSearchBar: IonSearchbar;
+
+  itemSearchBarSubscription: Subscription;
+
+  itemList: ButcheryBatch[] = [];
+
+  searchValue = '';
+
+  pageNumber = 0;
+  totalPages = 0;
+
+  isFetching = false;
+
+  constructor(
+    private butcheryBatchService: ButcheryBatchesService,
+    private config: AppParamsConfig,
+    private modalController: ModalController
+  ) {}
+
+  ngOnDestroy(): void {
+    this.itemSearchBarSubscription.unsubscribe();
+  }
+
+  ngOnInit() {
+    this.itemSearchBarSubscription = this.itemSearchBar.ionInput
+      .pipe(
+        map((event) => (event.target as HTMLInputElement).value),
+        debounceTime(this.config.waitTime),
+        distinctUntilChanged()
+      )
+      .subscribe((res) => {
+        this.searchValue = res.trim();
+        this.infiniteScroll.disabled = false;
+        this.itemList = [];
+        this.pageNumber = 0;
+        this.totalPages = 0;
+        if (this.searchValue) {
+          this.getBatches(undefined, 0, this.config.pageSize, this.searchValue);
+        } else {
+          this.getBatches(undefined, 0, this.config.pageSize);
+        }
+      });
+  }
+
+  ionViewDidEnter(): void {
+    setTimeout(() => {
+      this.itemSearchBar.setFocus();
+    }, 5);
+  }
+
+  getBatches(
+    event?,
+    pageNumber?: number,
+    pageSize?: number,
+    warehouseName?: string
+  ) {
+    this.isFetching = true;
+
+    if (warehouseName === undefined) {
+      this.butcheryBatchService
+        .getButcheryBatches(pageNumber, pageSize, undefined, undefined, [true])
+        .subscribe({
+          next: this.procressResult(event),
+          error: (error) => {
+            this.isFetching = false;
+          },
+        });
+    } else {
+      this.butcheryBatchService
+        .getButcheryBatches(pageNumber, pageSize, warehouseName, undefined, [true])
+        .subscribe({
+          next: this.procressResult(event),
+          error: (error) => {
+            this.isFetching = false;
+          },
+        });
+    }
+  }
+
+  procressResult(event?) {
+    return (data) => {
+      this.itemList = this.itemList.concat(data._embedded.butcheryBatches);
+      this.totalPages = data.page.totalPages;
+      this.isFetching = false;
+      if (event) {
+        event.target.complete();
+      }
+    };
+  }
+
+  loadMore(event) {
+    if (this.pageNumber + 1 >= this.totalPages) {
+      event.target.disabled = true;
+      return;
+    }
+
+    this.pageNumber++;
+
+    if (this.searchValue) {
+      this.getBatches(
+        event,
+        this.pageNumber,
+        this.config.pageSize,
+        this.searchValue
+      );
+    } else {
+      this.getBatches(event, this.pageNumber, this.config.pageSize);
+    }
+  }
+
+  dismissModal() {
+    this.modalController.dismiss(null, 'dismissModal');
+  }
+
+  onSelectBatch(batch: ButcheryBatch) {
+    this.modalController.dismiss(batch, 'butcheryBatch');
+  }
+}
