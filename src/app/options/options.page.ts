78de1068f0dd0f96a1bd5218e41b641f527646e3
Mark Lauron <malauron@gmail.com>
2023-12-10 23:57:47 +0800

    Barchart is finally done and inserted a meta tag in index.html that prevents client browsers from caching files from the server.

--- src/app/options/options.page.ts
+++ src/app/options/options.page.ts
@@ -43,19 +43,6 @@ export class OptionsPage implements OnInit {
 
             this.drawChart(newReleasing);
 
-            // const metrics = ['totalAmount', 'totalWeightKg'];
-
-            // let ctr = 0;
-            // setInterval(() => {
-            //   if (ctr >= metrics.length) {
-            //     ctr = 0;
-            //   }
-            // console.log(metrics[ctr]);
-
-            //   this.drawChart(newReleasing, metrics[ctr]);
-
-            //   ctr++;
-            // }, 5000);
           });
       });
   }
@@ -146,7 +133,28 @@ export class OptionsPage implements OnInit {
     return duumyDates;
   }
 
+  // Draw bar chart for releasing summary
   drawChart(releasingSummary: ButcheryReleasingSummaryDto[]) {
+
+    // Define Metrics
+    const metrics = [
+      {
+        xMetric: 'dateCreated',
+        yMetric: 'totalAmount',
+        yAxisLabel: '↑ Values in Php',
+        maxBuffer: 1.05,
+      },
+      {
+        xMetric: 'dateCreated',
+        yMetric: 'totalWeightKg',
+        yAxisLabel: '↑ Weight in Kilograms',
+        maxBuffer: 1.2,
+      },
+    ];
+
+    let currentMetric = metrics[1];
+
+    // Define dimension
     const dimensions = {
       width: 2000,
       height: 500,
@@ -158,22 +166,9 @@ export class OptionsPage implements OnInit {
     dimensions.ctrWidth = dimensions.width - dimensions.margins * 2;
     dimensions.ctrHeight = dimensions.height - dimensions.margins * 2;
 
-    // Define Metrics
-    const metrics = [
-      {
-        'xMetric': 'dateCreated',
-        'yMetric': 'totalAmount',
-        'yAxisLabel': '↑ Values in Php',
-        'maxBuffer': 1.05
-      },
-      {
-        'xMetric': 'dateCreated',
-        'yMetric': 'totalWeightKg',
-        'yAxisLabel' : '↑ Weight in Kilograms',
-        'maxBuffer': 1.2
-      }];
-
-    let currentMetric = metrics[1];
+    const rectPad = 15;
+    let textHeight = 0;
+    let textWidth = 0;
 
     // Draw Image
     const svg = d3
@@ -184,65 +179,117 @@ export class OptionsPage implements OnInit {
     // Container/Bounds Element
     const ctr = svg
       .append('g')
+      .attr('fill', 'none')
       .attr(
         'transform',
         `translate(${dimensions.margins}, ${dimensions.margins - 50})`
       );
 
+    // Barchart Title
+    ctr.append('g')
+        .append('text')
+        .text('Butcery releasings for the past 30 days.')
+        .attr('x', dimensions.ctrWidth/2)
+        .attr('y', 30)
+        .attr('text-anchor', 'middle')
+        .attr('fill', 'var(--ion-color-primary)')
+        .attr('font-size', '20px');
+
     // Bar elements
     ctr.append('g')
-        .attr('class', 'bars');
+      .attr('class', 'bars')
+      .attr('fill', 'steelblue');
 
-    // X-Axis element
+    // X-Axis elements
     ctr.append('g')
-        .attr('class', 'x-axis')
-        .style('transform', `translateY(${dimensions.ctrHeight}px)`);
+      .attr('class', 'x-axis')
+      .style('transform', `translateY(${dimensions.ctrHeight}px)`);
 
-    // Y-Axis element
+    // Y-Axis elements
     ctr.append('g')
-        .attr('class', 'y-axis')
-        .attr('transform', `translate(${dimensions.margins}, 0)`)
+      .attr('class', 'y-axis')
+      .attr('transform', `translate(${dimensions.margins}, 0)`)
 
       // Y-Axis label
-        .append('text')
-        .attr('class', 'y-axis-label')
-        .attr('x', -dimensions.margins)
-        .attr('y', 30)
-        .attr('fill', 'var(--ion-color-medium)')
-        .attr('text-anchor', 'start')
-        // .text('↑ Weight in Kilograms')
-        .attr('font-size', '15px');
+      .append('text')
+      .attr('class', 'y-axis-label')
+      .attr('x', 0)
+      .attr('y', 30)
+      .attr('fill', 'var(--ion-color-medium)')
+      .attr('text-anchor', 'middle')
+      .attr('font-size', '15px');
 
-
-      //   .append('text')
-      //   .attr('x', (dimensions.ctrWidth + dimensions.margins * 4) / 2)
-      //   .attr('y', 30)
-      //   .attr('fill', 'var(--ion-color-medium)')
-      //   .text('Butchery releasing summary for the past 30 days.')
-      //   .attr('font-size', '20px')
-      //   .attr('font-weight', 'bold')
+    // Mean group
+    const meanGroup = ctr.append('g')
+        .attr('class', 'mean-group');
 
     // Draw the mean line
-    const meanLine = ctr
+    const meanLine = meanGroup
       .append('line')
       .attr('x1', dimensions.margins)
       .attr('x2', dimensions.ctrWidth)
       .attr('y1', dimensions.ctrHeight)
-      .attr('y2', dimensions.ctrHeight);
+      .attr('y2', dimensions.ctrHeight)
+      .attr('stroke', 'var(--ion-color-primary)')
+      .style('stroke-dasharray', '6px 8px');
 
+    // Mean rectable
+    const meanRect = meanGroup.append('rect')
+        .attr('class', 'mean-rect')
+        .attr('x', dimensions.margins)
+        .attr('y', dimensions.ctrHeight)
+        .attr('fill', 'white')
+        .attr('stroke', 'var(--ion-color-primary)')
+        .style('stroke-dasharray', '6px 8px');
+
+    // Mean text
+    const meanText = meanGroup
+        .append('text')
+        .attr('class', 'mean-text')
+        .attr('transform', `translate(${dimensions.margins}, ${dimensions.ctrHeight})`)
+        .attr('fill', 'var(--ion-color-primary)')
+        .attr('text-anchor', 'end')
+        .attr('font-size', '15px');
+
+    // Tooltip
+    const tooltip = ctr.append('g')
+      .attr('class', 'tooltip')
+      .attr('opacity', '0');
+
+    tooltip.append('rect')
+      .attr('class', 'tooltip-rect')
+      .attr('stroke', 'grey')
+      .attr('fill', 'white');
+
+    tooltip.append('text')
+      .attr('class', 'tooltip-value')
+      .attr('fill', 'var(--ion-color-primary)')
+      .attr('stroke', 'var(--ion-color-primary)')
+      .attr('text-anchor', 'middle')
+      .attr('font-size', '15px');
+
+    //Define the scales
     const xScale = d3.scaleBand();
-    const yScale = d3 .scaleLinear();
+    const yScale = d3.scaleLinear();
+
+    // Define Axes
+    const xAxis = d3.axisBottom(xScale).tickSizeOuter(0);
+    const yAxis = d3.axisLeft(yScale);
 
     const drawImg = () => {
 
       // Accessor function
       const xAccessor = (d) => d[currentMetric.xMetric];
+
       // const yAccessor = (d) => d[yMetric];
       const yAccessor = (d) => d[currentMetric.yMetric];
 
       // Define Scales
       yScale
-        .domain([0, d3.max(releasingSummary, yAccessor) * currentMetric.maxBuffer])
+        .domain([
+          0,
+          d3.max(releasingSummary, yAccessor) * currentMetric.maxBuffer,
+        ])
         .rangeRound([dimensions.ctrHeight, dimensions.margins]);
 
       xScale
@@ -250,57 +297,16 @@ export class OptionsPage implements OnInit {
         .range([dimensions.margins, dimensions.ctrWidth])
         .padding(0.1);
 
-      // Draw Bar Chart
-      // const barChart = ctr.append('g')
-      //   .attr('fill', 'steelblue')
-      //   .selectAll('rect')
-      //   .data(releasingSummary)
-      //   .join((enter) =>
-      //     enter
-      //       .append('rect')
-      //       .attr('x', (d) => xScale(xAccessor(d)))
-      //       .attr('y', dimensions.ctrHeight)
-      //       .attr('width', xScale.bandwidth())
-      //       .attr('height', 0)
-
-      //       // Mouse hover effects
-      //       .on('mouseenter', function(d, i) {
-      //         d3.select(this)
-      //           .transition()
-      //           .duration('50')
-      //           .attr('opacity', '0.6');
-      //       })
-      //       .on('mouseout', function(d, i) {
-      //         d3.select(this).transition().duration('50').attr('opacity', '1');
-      //       })
-      //   );
-
-
-      const barChart = ctr.select('.bars')
-        .attr('fill', 'steelblue')
-        .selectAll('rect')
+      const barChart = ctr.select('.bars').selectAll('rect')
         .data(releasingSummary)
-        .join((enter) =>
-          enter
-            .append('rect')
-            .attr('x', (d) => xScale(xAccessor(d)))
-            .attr('y', dimensions.ctrHeight)
-            .attr('width', xScale.bandwidth())
-            .attr('height', 0)
-
-            // Mouse hover effects
-            .on('mouseover', function(d, i) {
-              d3.select(this)
-                // .transition()
-                // .duration('50')
-                .attr('opacity', '0.6');
-            })
-            .on('mouseout', function(d, i) {
-              d3.select(this)
-                // .transition()
-                // .duration('50')
-                .attr('opacity', '1');
-            })
+        .join(
+          (enter) =>
+            enter
+              .append('rect')
+              .attr('x', (d) => xScale(xAccessor(d)))
+              .attr('y', dimensions.ctrHeight)
+              .attr('width', xScale.bandwidth())
+              .attr('height', 0)
         );
 
       barChart
@@ -311,27 +317,8 @@ export class OptionsPage implements OnInit {
         .attr('width', xScale.bandwidth())
         .attr('height', (d) => yScale(0) - yScale(yAccessor(d)));
 
-      barChart.select('title').remove();
-
-      barChart
-        .append('title')
-        .text((d) =>
-          d[currentMetric.yMetric].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-        );
-
-      // Draw Axes
-      const xAxis = d3.axisBottom(xScale).tickSizeOuter(0);
-      const yAxis = d3.axisLeft(yScale);
-
-      // const xAxisGroup = ctr
-      //   .append('g')
-      //   .call(xAxis)
-      //   .style('transform', `translateY(${dimensions.ctrHeight}px)`);
-
-      const xAxisGroup = ctr
-        .select('.x-axis')
-        .transition()
-        .call(xAxis);
+      // Draw aces
+      const xAxisGroup = ctr.select('.x-axis').transition().call(xAxis);
 
       xAxisGroup
         .selectAll('text')
@@ -340,65 +327,103 @@ export class OptionsPage implements OnInit {
         .attr('transform', `translate(-10,0)rotate(-45)`)
         .style('text-anchor', 'end');
 
-      // xAxisGroup.append('text')
-      //     .attr('x', dimensions.ctrWidth / 2)
-      //     .attr('y', -dimensions.ctrHeight + 45)
-      //     .attr('fill', 'var(--ion-color-medium)')
-      //     .text('Butchery releasing summary for the past 30 days.')
-      //     .attr('font-size', '20px')
-      //     .attr('font-weight', 'bold');
-
-      // const yAxisGroup = ctr
-      //   .append('g')
-      //   .attr('transform', `translate(${dimensions.margins}, 0)`)
-      //   .call(yAxis);
-
       const yAxisGroup = ctr
         .select('.y-axis')
         .transition()
         .duration(2000)
         .call(yAxis);
 
-      // yAxisGroup.select('.domain').remove();
-      yAxisGroup.select('.domain')
-        .attr('opacity','0');
+      yAxisGroup.select('.domain').attr('opacity', '0');
 
-      const yAxisLabel = ctr.select('.y-axis-label')
-            .text(currentMetric.yAxisLabel);
+      // Update y-axis label
+      ctr.select('.y-axis-label')
+        .text(currentMetric.yAxisLabel);
 
       yAxisGroup.selectAll('text').attr('font-size', '15px');
-
-      // Graph Title
-      // yAxisGroup
-      //   .append('text')
-      //   .attr('x', (dimensions.ctrWidth + dimensions.margins * 4) / 2)
-      //   .attr('y', 30)
-      //   .attr('fill', 'var(--ion-color-medium)')
-      //   .text('Butchery releasing summary for the past 30 days.')
-      //   .attr('font-size', '20px')
-      //   .attr('font-weight', 'bold');
-
-      // // Y-Axis label
-      // yAxisGroup
-      //   .append('text')
-      //   .attr('x', -dimensions.margins)
-      //   .attr('y', 30)
-      //   .attr('fill', 'var(--ion-color-medium)')
-      //   .attr('text-anchor', 'start')
-      //   .text('↑ Weight in Kilograms')
-      //   .attr('font-size', '15px');
 
       // Mean/Average
       const mean = d3.mean(releasingSummary, yAccessor);
 
+      // Draw mean line
       meanLine
         .transition()
         .duration(1000)
         .attr('y1', yScale(mean))
-        .attr('y2', yScale(mean))
-        .attr('stroke', 'var(--ion-color-medium)')
-        .style('stroke-dasharray', '6px 8px');
+        .attr('y2', yScale(mean));
 
+      // Update mean value
+      meanText
+        .text(d3.format(',.0f')(mean))
+        .attr('opacity', '0')
+        .attr('transform', `translate(${dimensions.margins - rectPad / 2}, ${yScale(mean) + (rectPad-3) / 2})`)
+        .transition().duration(2000)
+        .attr('opacity', '1');
+
+      // Get text width & height
+      meanText.each(function(v) {
+        textHeight = this.getBBox().height + rectPad;
+        textWidth = this.getBBox().width + rectPad;
+      });
+
+      // Update text container postition
+      meanRect
+        .transition()
+        .duration(1000)
+        .attr('width', textWidth)
+        .attr('height', textHeight)
+        .attr('x', dimensions.margins - textWidth)
+        .attr('y', yScale(mean) - textHeight / 2);
+
+      // Hide tooltip
+      tooltip.attr('opacity', '0');
+
+      // Bar mouse hover event
+      const onMouseEnter = (event, d) => {
+
+        tooltip.attr('opacity', '0');
+
+        const tolltipXPos = xScale(xAccessor(d)) + xScale.bandwidth() / 2;
+        const tooltipYPos = yScale(yAccessor(d)) - 20;
+
+        const tooltipValue = tooltip
+          .select('.tooltip-value')
+          .text(d3.format(',.2f')(d[currentMetric.yMetric]))
+          .attr('transform', `translate(${tolltipXPos}, ${tooltipYPos})`);
+
+        // Get text widt
+        tooltipValue.each(function(v) {
+          textWidth = this.getBBox().width + rectPad;
+        });
+
+        tooltip
+          .select('.tooltip-rect')
+          .attr('x', tolltipXPos - textWidth / 2)
+          .attr('y', tooltipYPos - 20)
+          .attr('width', textWidth)
+          .attr('height', rectPad * 2);
+
+        tooltip
+          .transition()
+          .duration(100)
+          .attr('opacity', '1');
+
+        // Change bars opacity
+        d3.select(event.target)
+          .attr('opacity', '0.6');
+      };
+
+      // Bar mouse leave event
+      const onMouseLeave = (event, d) => {
+        tooltip.attr('opacity', '0');
+
+        // Change bars opacity
+        d3.select(event.target)
+          .attr('opacity', '1');
+      };
+
+      barChart
+        .on('mouseenter', onMouseEnter)
+        .on('mouseleave', onMouseLeave);
     };
 
     drawImg();
@@ -416,6 +441,5 @@ export class OptionsPage implements OnInit {
 
       row++;
     }, 10000);
-
   }
 }
